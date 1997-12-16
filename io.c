97d6916e2df3d6f935e4d57f162a44a77377064c
Andrew Tridgell <tridge@samba.org>
1997-12-16 09:25:17 +0000

    #if 0 the write exception code for the moment. I need to work out why it gets a successful write select on a fd followed by a EAGAIN write yet the fd is still OK.

--- io.c
+++ io.c
@@ -252,8 +252,13 @@ static int writefd(int fd,char *buf,int len)
       return -1;
 
     if (ret == -1 && got_select) {
+	    /* hmmm, we got a write select on the fd and then failed to write.
+	       Why doesn't that mean that the fd is dead? It doesn't on some
+	       systems it seems (eg. IRIX) */
+#if 0
 	    fprintf(FERROR,"write exception\n");
 	    exit_cleanup(1);
+#endif
     }
 
     got_select = 0;
