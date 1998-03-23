1aa71c8d5784c131688c57ff8d3c683a02f43d0c
Andrew Tridgell <tridge@samba.org>
1998-03-23 06:26:08 +0000

    don't adapt the block size to above half the chunk size

--- rsync.c
+++ rsync.c
@@ -283,6 +283,7 @@ int adapt_block_size(struct file_struct *file, int bsize)
 	int ret = file->length / (10000); /* rough heuristic */
 	ret = ret & ~15; /* multiple of 16 */
 	if (ret < bsize) ret = bsize;
+	if (ret > CHUNK_SIZE/2) ret = CHUNK_SIZE/2;
 	return ret;
 }
 
