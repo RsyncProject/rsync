f28ee65baadf611e7fb95e8ac1f795306da0ec1f
Andrew Tridgell <tridge@samba.org>
1998-05-06 07:35:37 +0000

    damn solaris doesn't prototype it's functions correctly. grrr.

--- syscall.c
+++ syscall.c
@@ -128,6 +128,7 @@ int do_fstat(int fd, STRUCT_STAT *st)
 OFF_T do_lseek(int fd, OFF_T offset, int whence)
 {
 #if HAVE_OFF64_T
+	off64_t lseek64();
 	return lseek64(fd, offset, whence);
 #else
 	return lseek(fd, offset, whence);
