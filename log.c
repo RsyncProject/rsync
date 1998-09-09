e0414f42027769676cfd0a119eab2fa8e62f8e0f
Andrew Tridgell <tridge@samba.org>
1998-09-09 06:06:08 +0000

    put the time in when using log file.

--- log.c
+++ log.c
@@ -25,10 +25,33 @@
 
 static FILE *logfile;
 
+
+/****************************************************************************
+  return the date and time as a string
+****************************************************************************/
+static char *timestring(void )
+{
+	static char TimeBuf[200];
+	time_t t = time(NULL);
+	struct tm *tm = localtime(&t);
+
+#ifdef HAVE_STRFTIME
+	strftime(TimeBuf,sizeof(TimeBuf)-1,"%Y/%m/%d %T",tm);
+#else
+	strlcpy(TimeBuf, asctime(tm), sizeof(TimeBuf)-1);
+#endif
+
+	if (TimeBuf[strlen(TimeBuf)-1] == '\n') {
+		TimeBuf[strlen(TimeBuf)-1] == 0;
+	}
+
+	return(TimeBuf);
+}
+
 static void logit(int priority, char *buf)
 {
 	if (logfile) {
-		fprintf(logfile,"%s", buf);
+		fprintf(logfile,"%s  %s", timestring(), buf);
 		fflush(logfile);
 	} else {
 		syslog(priority, "%s", buf);
