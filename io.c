d730b113f4608a952f7d6ad6efc35a7cb11be18c
Andrew Tridgell <tridge@samba.org>
1998-10-29 05:48:11 +0000

    final change needed to get rsync working on a CRAY J90

--- io.c
+++ io.c
@@ -270,8 +270,12 @@ static void readfd(int fd,char *buffer,int N)
 int32 read_int(int f)
 {
 	char b[4];
+	int32 ret;
+
 	readfd(f,b,4);
-	return IVAL(b,0);
+	ret = IVAL(b,0);
+	if (ret == (int32)0xffffffff) return -1;
+	return ret;
 }
 
 int64 read_longint(int f)
