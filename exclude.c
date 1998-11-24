5d5811f7d9e64265130951007b14a6d88a2425b6
David Dykstra <dwd@samba.org>
1998-11-24 21:26:38 +0000

    Always include "." when processing exclude lists.  This avoids confusion when people do --exclude "*".  Also, add an example to the man page that shows explicitly including parent directories when itemizing specific paths to include followed by --exclude "*".

--- exclude.c
+++ exclude.c
@@ -160,6 +160,10 @@ int check_exclude(char *name,struct exclude_struct **local_exclude_list,
 {
 	int n;
 
+	if (name && (name[0] == '.') && !name[1])
+		/* never exclude '.', even if somebody does --exclude '*' */
+		return 0;
+
 	if (exclude_list) {
 		for (n=0; exclude_list[n]; n++)
 			if (check_one_exclude(name,exclude_list[n],st))
