117af10225d7efd286ffd77ff550644985ce0918
David Dykstra <dwd@samba.org>
1999-01-05 20:08:45 +0000

    Change the receive log message from "send" to "recv".  Fix from Rick Smith <rick@rbsmith.com>.

--- log.c
+++ log.c
@@ -289,9 +289,9 @@ void log_recv(struct file_struct *file, struct stats *initial_stats)
 	extern char *log_format;
 
 	if (lp_transfer_logging(module_id)) {
-		log_formatted(FLOG, lp_log_format(module_id), "send", file, initial_stats);
+		log_formatted(FLOG, lp_log_format(module_id), "recv", file, initial_stats);
 	} else if (log_format && !am_server) {
-		log_formatted(FINFO, log_format, "send", file, initial_stats);
+		log_formatted(FINFO, log_format, "recv", file, initial_stats);
 	}
 }
 
