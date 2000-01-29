9bec5286063b7e9210f945c68a738e96662a37ec
Andrew Tridgell <tridge@samba.org>
2000-01-29 02:35:01 +0000

    runtime detect fnmatch() bug if ** is used.
    its all too common to compile with a working libc and run on a broken
    one.

--- exclude.c
+++ exclude.c
@@ -52,7 +52,17 @@ static struct exclude_struct *make_exclude(char *pattern, int include)
 
 	if (strpbrk(pattern, "*[?")) {
 	    ret->regular_exp = 1;
-	    ret->fnmatch_flags = strstr(pattern, "**") ? 0 : FNM_PATHNAME;
+	    ret->fnmatch_flags = FNM_PATHNAME;
+	    if (strstr(pattern, "**")) {
+		    static int tested;
+		    if (!tested) {
+			    tested = 1;
+			    if (fnmatch("a/b/*", "a/b/c/d", FNM_PATHNAME)==0) {
+				    rprintf(FERROR,"WARNING: fnmatch FNM_PATHNAME is broken on your system\n");
+			    }
+		    }
+		    ret->fnmatch_flags = 0;
+	    }
 	}
 
 	if (strlen(pattern) > 1 && pattern[strlen(pattern)-1] == '/') {
