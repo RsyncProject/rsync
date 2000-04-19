14175f1e7700e4e800692fc2c466cbd27a60176e
Andrew Tridgell <tridge@samba.org>
2000-04-19 05:49:15 +0000

    fixed bug in replacement inet_aton()

--- lib/compat.c
+++ lib/compat.c
@@ -155,12 +155,12 @@
 
 	if (strcmp(cp, "255.255.255.255") == 0) {
 		inp->s_addr = (unsigned) -1;
-		return 1;
+		return 0;
 	}
 
 	if (sscanf(cp, "%u.%u.%u.%u", &a1, &a2, &a3, &a4) != 4 ||
 	    a1 > 255 || a2 > 255 || a3 > 255 || a4 > 255) {
-		return 1;
+		return 0;
 	}
 
 	ret = (a1 << 24) | (a2 << 16) | (a3 << 8) | a4;
