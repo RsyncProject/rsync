69c6522734897c0b81b184c5ebcc6cd9461f6da3
Andrew Tridgell <tridge@samba.org>
2000-06-24 13:20:21 +0000

    added *.bz2 and *.tbz to default dont compress list

--- loadparm.c
+++ loadparm.c
@@ -162,7 +162,7 @@ static service sDefault =
 	NULL,    /* include from */
 	"%o %h [%a] %m (%u) %f %l",    /* log format */
 	NULL,    /* refuse options */
-	"*.gz *.tgz *.zip *.z *.rpm *.deb *.iso",    /* dont compress */
+	"*.gz *.tgz *.zip *.z *.rpm *.deb *.iso *.bz2 *.tbz",    /* dont compress */
 	0,        /* timeout */
 	0        /* max connections */
 };
