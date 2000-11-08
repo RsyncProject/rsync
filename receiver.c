4e40377ac262b5f0a76b3fff08f0ad7ce58cf56c
Martin Pool <mbp@samba.org>
2000-11-08 09:32:10 +0000

    Better error messages when unlink fails

--- receiver.c
+++ receiver.c
@@ -82,14 +82,15 @@ static void delete_one(struct file_struct *f)
 {
 	if (!S_ISDIR(f->mode)) {
 		if (robust_unlink(f_name(f)) != 0) {
-			rprintf(FERROR,"unlink %s : %s\n",f_name(f),strerror(errno));
+			rprintf(FERROR,"delete_one: unlink %s: %s\n",f_name(f),strerror(errno));
 		} else if (verbose) {
 			rprintf(FINFO,"deleting %s\n",f_name(f));
 		}
 	} else {    
 		if (do_rmdir(f_name(f)) != 0) {
 			if (errno != ENOTEMPTY && errno != EEXIST)
-				rprintf(FERROR,"rmdir %s : %s\n",f_name(f),strerror(errno));
+				rprintf(FERROR,"delete_one: rmdir %s: %s\n",
+                                        f_name(f), strerror(errno));
 		} else if (verbose) {
 			rprintf(FINFO,"deleting directory %s\n",f_name(f));      
 		}
