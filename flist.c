efe3037cf5cfba379cffb4068775c75b5b0172db
Andrew Tridgell <tridge@samba.org>
2001-05-02 11:13:21 +0000

    use _S_IFLNK not S_IFLNK

--- flist.c
+++ flist.c
@@ -203,7 +203,7 @@ static void set_filesystem(char *fname)
 
 static int to_wire_mode(mode_t mode)
 {
-	if (S_ISLNK(mode) && (S_IFLNK != 0120000)) {
+	if (S_ISLNK(mode) && (_S_IFLNK != 0120000)) {
 		return (mode & ~(_S_IFMT)) | 0120000;
 	}
 	return (int)mode;
@@ -211,8 +211,8 @@ static int to_wire_mode(mode_t mode)
 
 static mode_t from_wire_mode(int mode)
 {
-	if ((mode & (_S_IFMT)) == 0120000 && (S_IFLNK != 0120000)) {
-		return (mode & ~(_S_IFMT)) | S_IFLNK;
+	if ((mode & (_S_IFMT)) == 0120000 && (_S_IFLNK != 0120000)) {
+		return (mode & ~(_S_IFMT)) | _S_IFLNK;
 	}
 	return (mode_t)mode;
 }
