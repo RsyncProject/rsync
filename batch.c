2db52650fccb4647349a3c5d51b9309bf4039c6c
Martin Pool <mbp@samba.org>
2001-08-15 06:50:46 +0000

    Fix another format cast.

--- batch.c
+++ batch.c
@@ -545,15 +545,16 @@ void show_flist(int index, struct file_struct **fptr)
 
     int i;
     for (i=0;i<index;i++) {
-       rprintf(FINFO, "flist->flags=%#x\n",fptr[i]->flags);  
-       rprintf(FINFO, "flist->modtime=%#x\n",fptr[i]->modtime);  
-       rprintf(FINFO, "flist->length=%.0f\n", (double) fptr[i]->length);  
-       rprintf(FINFO, "flist->mode=%#o\n", (int) fptr[i]->mode);  
-       rprintf(FINFO, "flist->basename=%s\n",fptr[i]->basename);
-       if (fptr[i]->dirname) 
-	       rprintf(FINFO, "flist->dirname=%s\n",fptr[i]->dirname);
-       if (fptr[i]->basedir) 
-	       rprintf(FINFO, "flist->basedir=%s\n",fptr[i]->basedir);
+	    rprintf(FINFO, "flist->flags=%#x\n",fptr[i]->flags);  
+	    rprintf(FINFO, "flist->modtime=%#lx\n",
+		    (long unsigned) fptr[i]->modtime);  
+	    rprintf(FINFO, "flist->length=%.0f\n", (double) fptr[i]->length);  
+	    rprintf(FINFO, "flist->mode=%#o\n", (int) fptr[i]->mode);  
+	    rprintf(FINFO, "flist->basename=%s\n",fptr[i]->basename);
+	    if (fptr[i]->dirname) 
+		    rprintf(FINFO, "flist->dirname=%s\n",fptr[i]->dirname);
+	    if (fptr[i]->basedir) 
+		    rprintf(FINFO, "flist->basedir=%s\n",fptr[i]->basedir);
     }
 }
 
