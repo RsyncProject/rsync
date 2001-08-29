64bd756832d93c0767e72a3432fda29b30d69405
Martin Pool <mbp@samba.org>
2001-08-29 07:23:30 +0000

    Add comment: cyeoh says that getpass is deprecated, because it may return a truncated password on some systems, and it is not in the LSB.

--- authenticate.c
+++ authenticate.c
@@ -272,6 +272,9 @@ void auth_client(int fd, char *user, char *challenge)
 	if (!user || !*user) return;
 
 	if (!(pass=getpassf(password_file)) && !(pass=getenv("RSYNC_PASSWORD"))) {
+		/* XXX: cyeoh says that getpass is deprecated, because
+		   it may return a truncated password on some systems,
+		   and it is not in the LSB. */
 		pass = getpass("Password: ");
 	}
 
