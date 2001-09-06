a4cf6bec192051aaa58c04398e647f1c1c90c9ea
Martin Pool <mbp@samba.org>
2001-09-06 04:56:55 +0000

    Fix comment.

--- runtests.sh
+++ runtests.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2001 by Martin Pool <mbp@samba.org>
 
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License version
-# 2.1 as published by the Free Software Foundation.
+# it under the terms of the GNU General Public License version
+# 2 as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
