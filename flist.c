2e7d19945c6fa8ff4a2dbd4561cc499e94ac56c4
Martin Pool <mbp@samba.org>
2002-01-25 10:12:02 +0000

    With -vv, when the file list grows, show a message.

--- flist.c
+++ flist.c
@@ -279,13 +279,26 @@ static char *flist_dir;
 static void flist_expand(struct file_list *flist)
 {
 	if (flist->count >= flist->malloced) {
+		size_t new_bytes;
+		void *new_ptr;
+		
 		if (flist->malloced < 1000)
 			flist->malloced += 1000;
 		else
 			flist->malloced *= 2;
-		flist->files = (struct file_struct **)
-			realloc(flist->files,
-				sizeof(flist->files[0]) * flist->malloced);
+
+		new_bytes = sizeof(flist->files[0]) * flist->malloced;
+		
+		new_ptr = realloc(flist->files, new_bytes);
+
+		if (verbose > 2) {
+			rprintf(FINFO, RSYNC_NAME ": expand file_list to %.0f bytes, did%s move\n",
+				(double) new_bytes,
+				(new_ptr == flist->files) ? " not" : "");
+		}
+		
+		flist->files = (struct file_struct **) new_ptr;
+
 		if (!flist->files)
 			out_of_memory("flist_expand");
 	}
