301c680fd7689527bf5078ca8139eaa5458009de
David Dykstra <dwd@samba.org>
2002-02-13 18:42:20 +0000

    Suggested patch from Jim Ogilvie <jogilvie@us.ibm.com> to print out the system error message when mkstemp fails.

--- receiver.c
+++ receiver.c
@@ -424,7 +424,7 @@ int recv_files(int f_in,struct file_list *flist,char *local_name,int f_gen)
 		   access because of a similar race condition. */
 		fd2 = do_mkstemp(fnametmp, file->mode & INITACCESSPERMS);
 		if (fd2 == -1) {
-			rprintf(FERROR,"mkstemp %s failed\n",fnametmp);
+			rprintf(FERROR,"mkstemp %s failed: %s\n",fnametmp,strerror(errno));
 			receive_data(f_in,buf,-1,NULL,file->length);
 			if (buf) unmap_file(buf);
 			continue;
