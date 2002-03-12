ac69049ec2c354299b260fb859d029609fba91e5
Martin Pool <mbp@samba.org>
2002-03-12 02:07:36 +0000

    Additional trivial merge patch from Jos

--- zlib/infblock.c
+++ zlib/infblock.c
@@ -1,5 +1,5 @@
 /* infblock.c -- interpret and process block types to last block
- * Copyright (C) 1995-1998 Mark Adler
+ * Copyright (C) 1995-2002 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
 
