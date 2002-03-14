c7b562becf9c926f65b9845ed6cd1c3a4635cf83
Martin Pool <mbp@samba.org>
2002-03-14 12:00:12 +0000

    Correct the plural in "1 file to consider."  (Greg Louis)

--- flist.c
+++ flist.c
@@ -101,11 +101,12 @@ static void finish_filelist_progress(const struct file_list *flist)
 {
 	if (do_progress) {
 		/* This overwrites the progress line */
-		rprintf(FINFO, "%d files to consider\n", flist->count);
-	} else
+		rprintf(FINFO, "%d file%sto consider\n",
+			flist->count, flist->count == 1 ? " " : "s ");
+	} else {
 		rprintf(FINFO, "done\n");
+	}
 }
-
 
 void show_flist_stats(void)
 {
