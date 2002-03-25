c127e8aaec2b0ea408a3cd3a36fd910520249332
Martin Pool <mbp@samba.org>
2002-03-25 03:51:17 +0000

    Add a test case for trim_trailing_slashes, and make it handle other cases.

--- syscall.c
+++ syscall.c
@@ -113,15 +113,19 @@ int do_rename(char *fname1, char *fname2)
 
 void trim_trailing_slashes(char *name)
 {
-	char *p;
+	int l;
 	/* Some BSD systems cannot make a directory if the name
 	 * contains a trailing slash.
 	 * <http://www.opensource.apple.com/bugs/X/BSD%20Kernel/2734739.html> */
-	if (!*name)
-		return;		/* empty string */
-	p = strchr(name, '\0') - 1;
-	while (p == '/') {
-		p-- = '\0';
+	
+	/* Don't change empty string; and also we can't improve on
+	 * "/" */
+	
+	l = strlen(name);
+	while (l > 1) {
+		if (name[--l] != '/')
+			break;
+		name[l] = '\0';
 	}
 }
 
---
+++ trimslash.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2002 by Martin Pool
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "rsync.h"
+
+/* These are to make syscall.o shut up. */
+int dry_run = 0;
+int read_only = 1;
+int list_only = 0;
+
+/**
+ * @file trimslash.c
+ *
+ * Test harness; not linked into release.
+ **/
+int main(int argc, char **argv)
+{
+	int i;
+	
+	if (argc <= 1) {
+		fprintf(stderr, "trimslash: needs at least one argument\n");
+		return 1;
+	}
+
+	for (i = 1; i < argc; i++) {
+		trim_trailing_slashes(argv[i]);	/* modify in place */
+		printf("%s\n", argv[i]);
+	}
+	return 0;
+}
