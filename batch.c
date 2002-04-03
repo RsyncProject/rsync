1bc209b441a7461f50fe4d0ceb8519d399e4d8e8
Martin Pool <mbp@samba.org>
2002-04-03 06:55:24 +0000

    Fix completely braindead previous patch.  Thanks Jos.

--- batch.c
+++ batch.c
@@ -405,14 +405,14 @@ void write_batch_csum_info(int *flist_entry, int flist_count,
 			   struct sum_struct *s)
 {
 	size_t i;
-	size_t int_count;
+	int int_count;
 	extern int csum_length;
 
 	fdb_open = 1;
 
 	write_batch_csums_file(flist_entry, sizeof(int));
-	int_count = s->count;
-	write_batch_csums_file(&int_count, int_count);
+	int_count = s ? (int) s->count : 0;
+	write_batch_csums_file(&int_count, sizeof int_count);
 	
 	if (s) {
 		for (i = 0; i < s->count; i++) {
