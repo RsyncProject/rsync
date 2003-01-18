75fb17b891da5568773ee741ca5266edebe268c2
Wayne Davison <wayned@samba.org>
2003-01-18 19:00:07 +0000

    Define DBL_EPSILON if it doesn't exist.

--- popt/popt.c
+++ popt/popt.c
@@ -904,6 +904,9 @@ int poptGetNextOpt(poptContext con)
 		    if ((opt->argInfo & POPT_ARG_MASK) == POPT_ARG_DOUBLE) {
 			*((double *) opt->arg) = aDouble;
 		    } else {
+#ifndef DBL_EPSILON
+#define DBL_EPSILON 2.2204460492503131e-16
+#endif
 #define _ABS(a)	((((a) - 0.0) < DBL_EPSILON) ? -(a) : (a))
 			if ((_ABS(aDouble) - FLT_MAX) > DBL_EPSILON)
 			    return POPT_ERROR_OVERFLOW;
