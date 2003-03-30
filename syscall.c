e106de49c83ec70eb8c8bf4c92fd493757847e04
Wayne Davison <wayned@samba.org>
2003-03-30 23:00:51 +0000

    Tweaked the O_BINARY code to the latest idiom.

--- syscall.c
+++ syscall.c
@@ -85,14 +85,10 @@ int do_open(char *pathname, int flags, mode_t mode)
 	    if (dry_run) return -1;
 	    CHECK_RO
 	}
-#ifdef O_BINARY
-	/* for Windows */
-	flags |= O_BINARY;
-#endif
 	/* some systems can't handle a double / */
 	if (pathname[0] == '/' && pathname[1] == '/') pathname++;
 
-	return open(pathname, flags, mode);
+	return open(pathname, flags | O_BINARY, mode);
 }
 
 #if HAVE_CHMOD
