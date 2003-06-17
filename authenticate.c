908f5a9f9fe6d587e7b97e449a835ee51a85e8d8
Martin Pool <mbp@samba.org>
2003-06-17 04:46:32 +0000

    Add a comment about using getpassphrase() or readpassphrase() rather than getpass().  No code change.

--- authenticate.c
+++ authenticate.c
@@ -273,8 +273,14 @@ void auth_client(int fd, char *user, char *challenge)
 
 	if (!(pass=getpassf(password_file)) && !(pass=getenv("RSYNC_PASSWORD"))) {
 		/* XXX: cyeoh says that getpass is deprecated, because
-		   it may return a truncated password on some systems,
-		   and it is not in the LSB. */
+		 * it may return a truncated password on some systems,
+		 * and it is not in the LSB.
+                 *
+                 * Andrew Klein says that getpassphrase() is present
+                 * on Solaris and reads up to 256 characters.
+                 *
+                 * OpenBSD has a readpassphrase() that might be more suitable.
+                 */
 		pass = getpass("Password: ");
 	}
 
