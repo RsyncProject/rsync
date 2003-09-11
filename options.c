b7b2741f3ae6a0bb58d6051940bf761b7d05b0b2
Wayne Davison <wayned@samba.org>
2003-09-11 04:53:01 +0000

    Tweaked an error message.

--- options.c
+++ options.c
@@ -397,7 +397,7 @@ void option_error(void)
 {
 	if (err_buf[0]) {
 		rprintf(FLOG, "%s", err_buf);
-		rprintf(FERROR, "%s: %s", RSYNC_NAME, err_buf);
+		rprintf(FERROR, RSYNC_NAME ": %s", err_buf);
 	} else {
 		rprintf (FERROR, "Error parsing options: "
 			 "option may be supported on client but not on server?\n");
