2990e06f29fb2bc1147c3670ab9a090c0f6b53a1
Wayne Davison <wayned@samba.org>
2004-01-03 01:12:10 +0000

    Silence a compiler warning.

--- generator.c
+++ generator.c
@@ -226,7 +226,7 @@ static BOOL disable_deltas_p(void)
  *
  * Generate approximately one checksum every block_len bytes.
  */
-static void generate_and_send_sums(struct map_struct *buf, OFF_T len, int f_out)
+static void generate_and_send_sums(struct map_struct *buf, size_t len, int f_out)
 {
 	size_t i;
 	struct sum_struct sum;
