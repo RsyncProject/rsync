4068d8617dd301f7e1590b0419c9d2e24103b1b4
Wayne Davison <wayned@samba.org>
2004-02-17 22:49:19 +0000

    We no longer munge a double-leading slash in do_open() because we shouldn't generate a path with a double-leading slash anymore.

--- syscall.c
+++ syscall.c
@@ -85,8 +85,6 @@ int do_open(char *pathname, int flags, mode_t mode)
 	    if (dry_run) return -1;
 	    CHECK_RO
 	}
-	/* some systems can't handle a double / */
-	if (pathname[0] == '/' && pathname[1] == '/') pathname++;
 
 	return open(pathname, flags | O_BINARY, mode);
 }
