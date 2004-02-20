40ae4f93a079ed71e491e6d51914d6d663ea3f10
Wayne Davison <wayned@samba.org>
2004-02-20 17:01:33 +0000

    Don't use NGROUPS_MAX define.

--- getgroups.c
+++ getgroups.c
@@ -26,26 +26,32 @@
 
 #include "rsync.h"
 
-#ifndef NGROUPS_MAX
-/* It ought to be defined, but just in case. */
-#  define NGROUPS_MAX 32
-#endif
-
 int
 main(UNUSED(int argc), UNUSED(char *argv[]))
 {
 	int n, i;
-	gid_t list[NGROUPS_MAX];
+	gid_t *list;
 	gid_t gid = MY_GID();
 	int gid_in_list = 0;
 
 #ifdef HAVE_GETGROUPS
-	if ((n = getgroups(NGROUPS_MAX, list)) < 0) {
+	if ((n = getgroups(0, NULL)) < 0) {
 		perror("getgroups");
 		return 1;
 	}
 #else
 	n = 0;
+#endif
+
+	list = (gid_t*)malloc(sizeof (gid_t) * (n + 1));
+	if (!list) {
+		fprintf(stderr, "out of memory!\n");
+		exit(1);
+	}
+
+#ifdef HAVE_GETGROUPS
+	if (n > 0)
+		n = getgroups(n, list);
 #endif
 
 	for (i = 0; i < n; i++)  {
