a174e1ed0fa38aa556f07988b330af3692a639e8
Wayne Davison <wayned@samba.org>
2004-02-25 21:20:59 +0000

    Tweaked some comments and some whitespace.

--- rsync.c
+++ rsync.c
@@ -143,15 +143,14 @@ int set_perms(char *fname,struct file_struct *file,STRUCT_STAT *st,
 	if (preserve_times && !S_ISLNK(st->st_mode) &&
 	    cmp_modtime(st->st_mtime, file->modtime) != 0) {
 		/* don't complain about not setting times on directories
-		   because some filesystems can't do it */
+		 * because some filesystems can't do it */
 		if (set_modtime(fname,file->modtime) != 0 &&
 		    !S_ISDIR(st->st_mode)) {
 			rprintf(FERROR, "failed to set times on %s: %s\n",
 				full_fname(fname), strerror(errno));
 			return 0;
-		} else {
-			updated = 1;
 		}
+		updated = 1;
 	}
 
 	change_uid = am_root && preserve_uid && st->st_uid != file->uid;
@@ -171,18 +170,18 @@ int set_perms(char *fname,struct file_struct *file,STRUCT_STAT *st,
 			}
 		}
 		if (do_lchown(fname,
-			      change_uid?file->uid:st->st_uid,
-			      change_gid?file->gid:st->st_gid) != 0) {
+		    change_uid ? file->uid : st->st_uid,
+		    change_gid ? file->gid : st->st_gid) != 0) {
 			/* shouldn't have attempted to change uid or gid
-			     unless have the privilege */
+			 * unless have the privilege */
 			rprintf(FERROR, "%s %s failed: %s\n",
-				change_uid ? "chown" : "chgrp",
-				full_fname(fname), strerror(errno));
+			    change_uid ? "chown" : "chgrp",
+			    full_fname(fname), strerror(errno));
 			return 0;
 		}
 		/* a lchown had been done - we have to re-stat if the
-                   destination had the setuid or setgid bits set due
-                   to the side effect of the chown call */
+                 * destination had the setuid or setgid bits set due
+                 * to the side effect of the chown call */
 		if (st->st_mode & (S_ISUID | S_ISGID)) {
 			link_stat(fname, st);
 		}
