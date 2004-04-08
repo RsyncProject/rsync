84a3efa0abf63d0e30909722f5b8331aa4052b4b
Wayne Davison <wayned@samba.org>
2004-04-08 23:15:39 +0000

    Simplify the setting of rdev & rdev_high in send_file_entry().

--- flist.c
+++ flist.c
@@ -367,13 +367,11 @@ void send_file_entry(struct file_struct *file, int f, unsigned short base_flags)
 			} else
 				rdev = 0;
 		} else if (IS_DEVICE(mode)) {
-			if ((file->u.rdev & ~(DEV64_T)0xFF) == rdev_high) {
+			rdev = file->u.rdev;
+			if ((rdev & ~(DEV64_T)0xFF) == rdev_high)
 				flags |= XMIT_SAME_HIGH_RDEV;
-				rdev = file->u.rdev;
-			} else {
-				rdev = file->u.rdev;
+			else
 				rdev_high = rdev & ~(DEV64_T)0xFF;
-			}
 		}
 	}
 	if (file->uid == uid)
