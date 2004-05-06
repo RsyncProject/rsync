c5bf99a1c27d4f0223558f68012a490069dbc76f
Wayne Davison <wayned@samba.org>
2004-05-06 21:29:36 +0000

    Correct a typo.

--- socket.c
+++ socket.c
@@ -373,7 +373,7 @@ static int *open_socket_in(int type, int port, const char *bind_address,
 #ifdef IPV6_V6ONLY
 		if (resp->ai_family == AF_INET6) {
 			setsockopt(s, IPPROTO_IPV6, IPV6_V6ONLY,
-				   (char *)&one, sizeof one) < 0;
+				   (char *)&one, sizeof one);
 		}
 #endif
 
