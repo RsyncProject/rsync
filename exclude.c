65e248709676a2633d1c7beb52b31eaeba174312
Wayne Davison <wayned@samba.org>
2004-05-22 05:32:20 +0000

    Fixed the comment for get_exclude_tok().

--- exclude.c
+++ exclude.c
@@ -235,10 +235,9 @@ int check_exclude(struct exclude_list_struct *listp, char *name, int name_is_dir
 /* Get the next include/exclude arg from the string.  The token will not
  * be '\0' terminated, so use the returned length to limit the string.
  * Also, be sure to add this length to the returned pointer before passing
- * it back to ask for the next token.  This routine will not parse the +/-
- * prefixes or the "!" token when xflags contains XFLG_WORDS_ONLY.  The
- * *incl_ptr value will be 1 for an include, 0 for an exclude, and -1 for
- * the list-clearing "!" token.
+ * it back to ask for the next token.  This routine parses the +/- prefixes
+ * and the "!" token unless xflags contains XFLG_WORDS_ONLY.  The *flag_ptr
+ * value will also be set to the MATCHFLG_* bits for the current token.
  */
 static const char *get_exclude_tok(const char *p, unsigned int *len_ptr,
 				   unsigned int *flag_ptr, int xflags)
