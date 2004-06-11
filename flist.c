ea76e761048fcc61ab26cdda31fa0c975446e1fa
Wayne Davison <wayned@samba.org>
2004-06-11 05:02:59 +0000

    In set_filesystem(), call do_stat(), not link_stat() to ensure that we get the directory behind it all.

--- flist.c
+++ flist.c
@@ -250,7 +250,7 @@ static dev_t filesystem_dev;
 static void set_filesystem(char *fname)
 {
 	STRUCT_STAT st;
-	if (link_stat(fname, &st) != 0)
+	if (do_stat(fname, &st) != 0)
 		return;
 	filesystem_dev = st.st_dev;
 }
