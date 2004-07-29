065a6052705d6a1f5405d863d1680b2201485908
Wayne Davison <wayned@samba.org>
2004-07-29 16:45:48 +0000

    Got rid of bogus compare_dest scan in skip_file() -- it must checksum the same file that we used for the stat() (in the parent routine).

--- generator.c
+++ generator.c
@@ -79,15 +79,6 @@ static int skip_file(char *fname, struct file_struct *file, STRUCT_STAT *st)
 	   of the file time to determine whether to sync */
 	if (always_checksum && S_ISREG(st->st_mode)) {
 		char sum[MD4_SUM_LENGTH];
-		char fnamecmpdest[MAXPATHLEN];
-
-		if (compare_dest != NULL) {
-			if (access(fname, 0) != 0) {
-				pathjoin(fnamecmpdest, sizeof fnamecmpdest,
-					 compare_dest, fname);
-				fname = fnamecmpdest;
-			}
-		}
 		file_checksum(fname,sum,st->st_size);
 		return memcmp(sum, file->u.sum, protocol_version < 21 ? 2
 							: MD4_SUM_LENGTH) == 0;
@@ -480,7 +471,7 @@ static void recv_generator(char *fname, struct file_struct *file, int i,
 		return;
 	}
 
-	if (skip_file(fname, file, &st)) {
+	if (skip_file(fnamecmp, file, &st)) {
 		if (fnamecmp == fname)
 			set_perms(fname, file, &st, PERMS_REPORT);
 		return;
