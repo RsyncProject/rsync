8b6ad0193d62ff7070f35cb6a3ecad7c34811c60
Wayne Davison <wayned@samba.org>
2004-07-30 20:05:37 +0000

    Don't ignore case in lp_number() because the rest of the daemon code can't handle the case where the module name doesn't exactly match the string the user provided.

--- loadparm.c
+++ loadparm.c
@@ -811,7 +811,7 @@ int lp_number(char *name)
    int iService;
 
    for (iService = iNumServices - 1; iService >= 0; iService--)
-      if (strequal(lp_name(iService), name))
+      if (strcmp(lp_name(iService), name) == 0)
          break;
 
    return (iService);
