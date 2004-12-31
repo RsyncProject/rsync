3dfe6e97a7e98c418479f5f0671f19c87f762697
Wayne Davison <wayned@samba.org>
2004-12-31 09:41:04 +0000

    Since lp_bind_address() can't return NULL, we need to check if it is an empty string and ignore it when it is.

--- clientserver.c
+++ clientserver.c
@@ -607,7 +607,7 @@ int daemon_main(void)
 
 	if (rsync_port == 0 && (rsync_port = lp_rsync_port()) == 0)
 		rsync_port = RSYNC_PORT;
-	if (bind_address == NULL)
+	if (bind_address == NULL && *lp_bind_address())
 		bind_address = lp_bind_address();
 
 	log_init();
