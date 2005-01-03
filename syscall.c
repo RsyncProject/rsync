051547603a1f094b5e21cd955ed00a1711ebf1fb
Wayne Davison <wayned@samba.org>
2005-01-03 21:03:33 +0000

    Moved kluged (conditional) define of lchown from rsync.h to syscall.c.

--- syscall.c
+++ syscall.c
@@ -72,6 +72,9 @@ int do_lchown(const char *path, uid_t owner, gid_t group)
 {
 	if (dry_run) return 0;
 	RETURN_ERROR_IF_RO_OR_LO;
+#ifdef HAVE_LCHOWN
+#define lchown chown
+#endif
 	return lchown(path, owner, group);
 }
 
