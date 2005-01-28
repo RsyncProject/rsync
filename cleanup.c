58fef0ac3850006697dca6478d57e75a168b0394
Wayne Davison <wayned@samba.org>
2005-01-28 20:41:36 +0000

    Changed direct call to fstat() into a call to do_fstat().

--- cleanup.c
+++ cleanup.c
@@ -37,12 +37,11 @@ void close_all(void)
 	int max_fd;
 	int fd;
 	int ret;
-	struct stat st;
+	STRUCT_STAT st;
 
 	max_fd = sysconf(_SC_OPEN_MAX) - 1;
 	for (fd = max_fd; fd >= 0; fd--) {
-		ret = fstat(fd,&st);
-		if (fstat(fd,&st) == 0) {
+		if ((ret = do_fstat(fd, &st)) == 0) {
 			if (is_a_socket(fd))
 				ret = shutdown(fd, 2);
 			ret = close(fd);
