422696201acf424dc0c46558f993d27e86bddd1b
Wayne Davison <wayned@samba.org>
2005-01-28 20:43:09 +0000

    Call the right stat function based on USE_STAT64_FUNCS.

--- getfsdev.c
+++ getfsdev.c
@@ -3,9 +3,15 @@
  int main(int argc, char *argv[])
 {
 	STRUCT_STAT st;
+	int ret;
 
 	while (--argc > 0) {
-		if (stat(*++argv, &st) < 0) {
+#if USE_STAT64_FUNCS
+		ret = stat64(*++argv, &st);
+#else
+		ret = stat(*++argv, &st);
+#endif
+		if (ret < 0) {
 			fprintf(stderr, "Unable to stat `%s'\n", *argv);
 			exit(1);
 		}
