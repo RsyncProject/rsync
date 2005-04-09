45c5b903eb96bc57ed1b4d59586defb1e4d0dc53
Wayne Davison <wayned@samba.org>
2005-04-09 18:11:25 +0000

    - Call auth_server() with its new "host" arg. - Don't log an auth-failed error -- auth_server() now handles that.

--- clientserver.c
+++ clientserver.c
@@ -268,11 +268,9 @@ static int rsync_module(int f_in, int f_out, int i)
 		return -1;
 	}
 
-	auth_user = auth_server(f_in, f_out, i, addr, "@RSYNCD: AUTHREQD ");
+	auth_user = auth_server(f_in, f_out, i, host, addr, "@RSYNCD: AUTHREQD ");
 
 	if (!auth_user) {
-		rprintf(FLOG, "auth failed on module %s from %s (%s)\n",
-			name, host, addr);
 		io_printf(f_out, "@ERROR: auth failed on module %s\n", name);
 		return -1;
 	}
