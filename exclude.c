98e4741463ea13c017e1d0776dbf3939da11d75a
Wayne Davison <wayned@samba.org>
2005-04-26 16:09:03 +0000

    A slightly better fix than the last one (though it won't make any difference in how the code behaves due to how it is called).

--- exclude.c
+++ exclude.c
@@ -1105,9 +1105,9 @@ static void send_rules(int f_out, struct filter_list_struct *flp)
 			continue;
 		if (ent->match_flags & MATCHFLG_CVS_IGNORE
 		    && !(ent->match_flags & MATCHFLG_MERGE_FILE)) {
-			int f = am_sender || protocol_version < 29 ? f_out : -1;
+			int f = am_sender || protocol_version < 29 ? f_out : -2;
 			send_rules(f, &cvs_filter_list);
-			if (f >= 0 || f_out < 0)
+			if (f == f_out)
 				continue;
 		}
 		p = get_rule_prefix(ent->match_flags, ent->pattern, 1, &plen);
