7eaabd8fac9e31e2fb749984730d830c6de5fe94
Wayne Davison <wayned@samba.org>
2006-02-03 23:40:37 +0000

    Decided to undo the last change to avoid backward compatibility inconsistencies (it would have been the right thing to do if it were done that way from the start, but not now).

--- options.c
+++ options.c
@@ -1368,8 +1368,7 @@ int parse_arguments(int *argc, const char ***argv, int frommain)
 				clean_fname(partial_dir, 1);
 			if (!*partial_dir || strcmp(partial_dir, ".") == 0)
 				partial_dir = NULL;
-			else if (*partial_dir != '/'
-			 && check_filter(&filter_list, partial_dir, 1) == 0) {
+			else if (*partial_dir != '/') {
 				parse_rule(&filter_list, partial_dir,
 				    MATCHFLG_NO_PREFIXES|MATCHFLG_DIRECTORY, 0);
 			}
