0be05d6038351c29f9bb7fafdc8d55c371ee6aeb
Wayne Davison <wayned@samba.org>
2006-02-05 07:20:10 +0000

    In default_charset(): if nl_langinfo() gives us "646", return "ascii".

--- rsync.c
+++ rsync.c
@@ -58,7 +58,10 @@ static char *default_charset(void)
 #if defined HAVE_LIBCHARSET_H && defined HAVE_LOCALE_CHARSET
 	return locale_charset();
 #elif defined HAVE_LANGINFO_H && defined HAVE_NL_LANGINFO
-	return nl_langinfo(CODESET);
+	char *def = nl_langinfo(CODESET);
+	if (strcmp(def, "646") == 0) /* Solaris brain-damage */
+		return "ascii";
+	return def;
 #else
 	return ""; /* Works with (at the very least) gnu iconv... */
 #endif
