0679ac4ca9d8fda7fc27dd0d44de48957c0ff88d
Wayne Davison <wayned@samba.org>
2006-02-08 02:32:35 +0000

    Tweaked a comment.

--- flist.c
+++ flist.c
@@ -1487,8 +1487,8 @@ void clear_file(struct file_struct *file, struct file_list *flist)
 	memset(file, 0, file_struct_len);
 	/* In an empty entry, dir.depth is an offset to the next non-empty
 	 * entry.  Likewise for length in the opposite direction.  We assume
-	 * that we're alone for now since flist_find() will collate adjacent
-	 * items for any entries that are encountered during the find. */
+	 * that we're alone for now since flist_find() will adjust the counts
+	 * it runs into that aren't up-to-date. */
 	file->length = file->dir.depth = 1;
 }
 
