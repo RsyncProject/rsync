a7704777cee14bb3d0cb3a288ee44a620fca8a59
Wayne Davison <wayned@samba.org>
2006-03-16 10:08:08 +0000

    An extra safeguard for msg2sndr_flush().

--- io.c
+++ io.c
@@ -1153,7 +1153,7 @@ static void msg2sndr_flush(void)
 	if (defer_forwarding_messages)
 		return;
 
-	while (msg2sndr.head) {
+	while (msg2sndr.head && io_multiplexing_out) {
 		struct msg_list_item *m = msg2sndr.head;
 		if (!(msg2sndr.head = m->next))
 			msg2sndr.tail = NULL;
