146d2228ccbbe64a7e5d457294c6b5493f1ff4ae
Wayne Davison <wayned@samba.org>
2006-04-13 16:52:15 +0000

    Got rid of the replacement inet_aton() and inet_ntoa() functions because no code in rsync has used these functions since 2.4.8.

--- lib/compat.c
+++ lib/compat.c
@@ -150,49 +150,6 @@
 }
 #endif
 
-#ifdef REPLACE_INET_NTOA
- char *rep_inet_ntoa(struct in_addr ip)
-{
-	unsigned char *p = (unsigned char *)&ip.s_addr;
-	static char buf[18];
-#ifdef WORDS_BIGENDIAN
-	snprintf(buf, 18, "%d.%d.%d.%d", 
-		 (int)p[0], (int)p[1], (int)p[2], (int)p[3]);
-#else
-	snprintf(buf, 18, "%d.%d.%d.%d", 
-		 (int)p[3], (int)p[2], (int)p[1], (int)p[0]);
-#endif
-	return buf;
-}
-#endif
-
-#ifdef REPLACE_INET_ATON
- int inet_aton(const char *cp, struct in_addr *inp)
-{
-	unsigned int a1, a2, a3, a4;
-	unsigned long ret;
-
-	if (strcmp(cp, "255.255.255.255") == 0) {
-		inp->s_addr = (unsigned) -1;
-		return 0;
-	}
-
-	if (sscanf(cp, "%u.%u.%u.%u", &a1, &a2, &a3, &a4) != 4 ||
-	    a1 > 255 || a2 > 255 || a3 > 255 || a4 > 255) {
-		return 0;
-	}
-
-	ret = (a1 << 24) | (a2 << 16) | (a3 << 8) | a4;
-
-	inp->s_addr = htonl(ret);
-	
-	if (inp->s_addr == (unsigned) -1) {
-		return 0;
-	}
-	return 1;
-}
-#endif
-
 /* some systems don't take the 2nd argument */
 int sys_gettimeofday(struct timeval *tv)
 {
