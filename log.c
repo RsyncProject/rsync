38de2866e5892ffc77a96636cd706239cbffbf4b
Wayne Davison <wayned@samba.org>
2006-05-03 06:11:11 +0000

    Made log_init() call timestring() instead of just localtime() prior to a chroot (note that timestring() makes use of localtime() plus strftime()).

--- log.c
+++ log.c
@@ -154,17 +154,14 @@ static void logfile_open(void)
 
 void log_init(void)
 {
-	time_t t;
-
 	if (log_initialised)
 		return;
 	log_initialised = 1;
 
-	/* this looks pointless, but it is needed in order for the
+	/* This looks pointless, but it is needed in order for the
 	 * C library on some systems to fetch the timezone info
-	 * before the chroot */
-	t = time(NULL);
-	localtime(&t);
+	 * before the chroot. */
+	timestring(time(NULL));
 
 	/* optionally use a log file instead of syslog */
 	logfname = lp_log_file();
