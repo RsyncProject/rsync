db0f7613e19253fce9de275ebf00bc04e23bc537
Wayne Davison <wayned@samba.org>
2006-11-28 19:34:01 +0000

    Got rid of an incorrect comment.

--- util.c
+++ util.c
@@ -1264,7 +1264,6 @@ void *_realloc_array(void *ptr, unsigned int size, unsigned long num)
 {
 	if (num >= MALLOC_MAX/size)
 		return NULL;
-	/* No realloc should need this, but just in case... */
 	if (!ptr)
 		return malloc(size * num);
 	return realloc(ptr, size * num);
