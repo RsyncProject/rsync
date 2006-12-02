20f0d0f9b9d4b373fdd2198a71d43a2c987e252a
Wayne Davison <wayned@samba.org>
2006-12-02 16:13:02 +0000

    Moved write_ndx_and_attrs() too.

--- rsync.c
+++ rsync.c
@@ -91,6 +91,19 @@ void setup_iconv()
 }
 #endif
 
+static void write_ndx_and_attrs(int f_out, int ndx, int iflags,
+				uchar fnamecmp_type, char *buf, int len)
+{
+	write_int(f_out, ndx);
+	if (protocol_version < 29)
+		return;
+	write_shortint(f_out, iflags);
+	if (iflags & ITEM_BASIS_TYPE_FOLLOWS)
+		write_byte(f_out, fnamecmp_type);
+	if (iflags & ITEM_XNAME_FOLLOWS)
+		write_vstring(f_out, buf, len);
+}
+
 /* This is used by sender.c with a valid f_out, and by receive.c with
  * f_out = -1. */
 int read_item_attrs(int f_in, int f_out, int ndx, uchar *type_ptr,
