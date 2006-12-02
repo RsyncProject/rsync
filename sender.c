d144e43bb3ea77bdc9d1dd68a648166970822dd6
Wayne Davison <wayned@samba.org>
2006-12-02 16:17:11 +0000

    Restored write_ndx_and_attrs() and made it public.

--- sender.c
+++ sender.c
@@ -143,6 +143,19 @@ void successful_send(int ndx)
 	}
 }
 
+void write_ndx_and_attrs(int f_out, int ndx, int iflags,
+			 uchar fnamecmp_type, char *buf, int len)
+{
+	write_int(f_out, ndx);
+	if (protocol_version < 29)
+		return;
+	write_shortint(f_out, iflags);
+	if (iflags & ITEM_BASIS_TYPE_FOLLOWS)
+		write_byte(f_out, fnamecmp_type);
+	if (iflags & ITEM_XNAME_FOLLOWS)
+		write_vstring(f_out, buf, len);
+}
+
 void send_files(struct file_list *flist, int f_out, int f_in)
 {
 	int fd = -1;
