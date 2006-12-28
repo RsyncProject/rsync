c2f0e4d965849a8860c5bb8f59ba548fc28805ec
Wayne Davison <wayned@samba.org>
2006-12-28 06:27:22 +0000

    Don't include MATCHFLG_PERISHABLE in CVS-exclude handling unless we're talking protocol 30 or above.

--- exclude.c
+++ exclude.c
@@ -879,14 +879,16 @@ static char default_cvsignore[] =
 
 static void get_cvs_excludes(uint32 mflags)
 {
-	char *p, fname[MAXPATHLEN];
 	static int initialized = 0;
+	char *p, fname[MAXPATHLEN];
 
 	if (initialized)
 		return;
 	initialized = 1;
 
-	parse_rule(&cvs_filter_list, default_cvsignore, mflags | MATCHFLG_PERISHABLE, 0);
+	parse_rule(&cvs_filter_list, default_cvsignore,
+		   mflags | (protocol_version >= 30 ? MATCHFLG_PERISHABLE : 0),
+		   0);
 
 	p = module_id >= 0 && lp_use_chroot(module_id) ? "/" : getenv("HOME");
 	if (p && pathjoin(fname, MAXPATHLEN, p, ".cvsignore") < MAXPATHLEN)
