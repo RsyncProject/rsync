8e41b68e8f975c02a5d9281be780ba5d1a385107
Wayne Davison <wayned@samba.org>
2007-07-10 13:55:49 +0000

    Tweaking the license text a bit more.

--- lib/sysxattrs.c
+++ lib/sysxattrs.c
@@ -5,8 +5,9 @@
  * Written by Jay Fenlason.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
