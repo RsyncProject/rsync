c605c2cdbd08c9f573bd4f0a0812643aa84979d7
Wayne Davison <wayned@samba.org>
2007-09-22 18:07:17 +0000

    Tweaked opening comments.

--- ifuncs.h
+++ ifuncs.h
@@ -1,4 +1,20 @@
-/* Inline functions. */
+/* Inline functions for rsync.
+ *
+ * Copyright (C) 2007 Wayne Davison
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, visit the http://fsf.org website.
+ */
 
 static inline void
 alloc_xbuf(xbuf *xb, size_t sz)
