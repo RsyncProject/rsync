e516b69ef67822cbbde7252cdddb1217b4aa5567
Wayne Davison <wayned@samba.org>
2007-11-05 15:02:30 +0000

    Fixed get_xattr_acl() -- it needed to zero *len_p.

--- xattrs.c
+++ xattrs.c
@@ -804,6 +804,7 @@ int set_xattr(const char *fname, const struct file_struct *file,
 char *get_xattr_acl(const char *fname, int is_access_acl, size_t *len_p)
 {
 	const char *name = is_access_acl ? XACC_ACL_ATTR : XDEF_ACL_ATTR;
+	*len_p = 0; /* no extra data alloc needed from get_xattr_data() */
 	return get_xattr_data(fname, name, len_p, 1);
 }
 
