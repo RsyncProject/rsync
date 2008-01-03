62a6b8df72d18f0b13462ee7ac33f9a8a46ef0b5
Wayne Davison <wayned@samba.org>
2008-01-02 17:20:44 -0800

    Made read_arg_from_pipe() handle EINTR.

--- clientserver.c
+++ clientserver.c
@@ -355,8 +355,12 @@ static int read_arg_from_pipe(int fd, char *buf, int limit)
 	char *bp = buf, *eob = buf + limit - 1;
 
 	while (1) {
-	    if (read(fd, bp, 1) != 1)
+	    int got = read(fd, bp, 1);
+	    if (got != 1) {
+		if (got < 0 && errno == EINTR)
+			continue;
 		return -1;
+	    }
 	    if (*bp == '\0')
 		break;
 	    if (bp < eob)
