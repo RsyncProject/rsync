a43ff267e9b6a902e51249e728ef0be22c05b462
Wayne Davison <wayned@samba.org>
2008-02-19 11:52:00 -0800

    Tweaked a comparison that "checker" was complaining about.

--- generator.c
+++ generator.c
@@ -565,7 +565,7 @@ int unchanged_attrs(const char *fname, struct file_struct *file, stat_x *sxp)
 	if (preserve_perms && !BITS_EQUAL(sxp->st.st_mode, file->mode, CHMOD_BITS))
 		return 0;
 
-	if (preserve_executability && ((sxp->st.st_mode & 0111) != 0) ^ ((file->mode & 0111) != 0))
+	if (preserve_executability && ((sxp->st.st_mode & 0111 ? 1 : 0) ^ (file->mode & 0111 ? 1 : 0)))
 		return 0;
 
 	if (am_root && uid_ndx && sxp->st.st_uid != (uid_t)F_OWNER(file))
