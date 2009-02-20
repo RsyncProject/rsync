e8d6fe62614f1bd21a41aa779188d9d0ec57de22
Wayne Davison <wayned@samba.org>
2009-02-19 23:09:20 -0800

    Properly indent some lines.

--- clientserver.c
+++ clientserver.c
@@ -387,16 +387,16 @@ static int read_arg_from_pipe(int fd, char *buf, int limit)
 	char *bp = buf, *eob = buf + limit - 1;
 
 	while (1) {
-	    int got = read(fd, bp, 1);
-	    if (got != 1) {
-		if (got < 0 && errno == EINTR)
-			continue;
-		return -1;
-	    }
-	    if (*bp == '\0')
-		break;
-	    if (bp < eob)
-		bp++;
+		int got = read(fd, bp, 1);
+		if (got != 1) {
+			if (got < 0 && errno == EINTR)
+				continue;
+			return -1;
+		}
+		if (*bp == '\0')
+			break;
+		if (bp < eob)
+			bp++;
 	}
 	*bp = '\0';
 
