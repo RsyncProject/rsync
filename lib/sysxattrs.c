2c1aa2efac42511903d37cbcf4eb54a731b9de7c
Wayne Davison <wayned@samba.org>
2009-09-03 15:25:55 -0700

    Need to use O_RDONLY in solaris sys_lremovexattr().

--- lib/sysxattrs.c
+++ lib/sysxattrs.c
@@ -217,7 +217,7 @@ int sys_lremovexattr(const char *path, const char *name)
 	int attrdirfd;
 	int ret;
 
-	if ((attrdirfd = attropen(path, ".", O_RDWR)) < 0)
+	if ((attrdirfd = attropen(path, ".", O_RDONLY)) < 0)
 		return -1;
 
 	ret = unlinkat(attrdirfd, name, 0);
