bdc038fce0ae9f0cb83b579608e3a244703f8cc7
Wayne Davison <wayned@samba.org>
2009-12-16 13:32:03 -0800

    Use 0-length MSG_DATA when MSG_NOOP is not available (is both safer and supports older rsyncs).

--- io.c
+++ io.c
@@ -949,14 +949,10 @@ void maybe_send_keepalive(void)
 {
 	if (time(NULL) - last_io_out >= allowed_lull) {
 		if (!iobuf_out || !iobuf_out_cnt) {
-			if (protocol_version < 29)
-				return; /* there's nothing we can do */
 			if (protocol_version >= 30)
 				send_msg(MSG_NOOP, "", 0, 0);
-			else {
-				write_int(sock_f_out, cur_flist->used);
-				write_shortint(sock_f_out, ITEM_IS_NEW);
-			}
+			else
+				send_msg(MSG_DATA, "", 0, 0);
 		}
 		if (iobuf_out)
 			io_flush(NORMAL_FLUSH);
