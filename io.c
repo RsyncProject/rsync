96cce18ce67f132a851c4e37b03fd09b8683233a
Wayne Davison <wayned@samba.org>
2009-12-21 13:48:44 -0800

    Defer forwarding messages during the forwarding of flist data.

--- io.c
+++ io.c
@@ -970,11 +970,13 @@ void start_flist_forward(int f_in)
 	assert(iobuf_out != NULL);
 	assert(iobuf_f_out == msg_fd_out);
 	flist_forward_from = f_in;
+	defer_forwarding_messages++;
 }
 
-void stop_flist_forward()
+void stop_flist_forward(void)
 {
 	flist_forward_from = -1;
+	defer_forwarding_messages--;
 	io_flush(FULL_FLUSH);
 }
 
