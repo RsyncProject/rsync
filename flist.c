fe2c582af879a633c346d0c36da3ca79f5d7ff3b
Wayne Davison <wayned@samba.org>
2010-03-31 14:56:52 -0700

    Removing now-redundant path-size check from send_if_directory().

--- flist.c
+++ flist.c
@@ -1476,12 +1476,6 @@ static void send_if_directory(int f, struct file_list *flist,
 		unsigned int len = strlen(fbuf);
 		if (len > 1 && fbuf[len-1] == '/')
 			fbuf[--len] = '\0';
-		if (len >= MAXPATHLEN - 1) {
-			io_error |= IOERR_GENERAL;
-			rprintf(FERROR_XFER, "skipping long-named directory: %s\n",
-				full_fname(fbuf));
-			return;
-		}
 		save_filters = push_local_filters(fbuf, len);
 		send_directory(f, flist, fbuf, len, flags);
 		pop_local_filters(save_filters);
