24a743b5650ad27c2db4a6323f714e072cdc595e
Wayne Davison <wayned@samba.org>
2010-07-03 08:54:40 -0700

    If a module has no path setting, return an error.

--- clientserver.c
+++ clientserver.c
@@ -509,6 +509,11 @@ static int rsync_module(int f_in, int f_out, int i, char *addr, char *host)
 	 * supplementary groups. */
 
 	module_dir = lp_path(i);
+	if (*module_dir == '\0') {
+		rprintf(FLOG, "No path specified for module %s\n", name);
+		io_printf(f_out, "@ERROR: no path setting.\n");
+		return -1;
+	}
 	if (use_chroot) {
 		if ((p = strstr(module_dir, "/./")) != NULL) {
 			*p = '\0'; /* Temporary... */
