86e90c58f46dd13254e0dc38b9eea37589ace457
Wayne Davison <wayned@samba.org>
2011-02-17 22:07:20 -0800

    Add .hg dir exclude to default_cvsignore list. Fixes bug 7957.

--- exclude.c
+++ exclude.c
@@ -1036,7 +1036,7 @@ static char default_cvsignore[] =
 	" *.a *.olb *.o *.obj *.so *.exe"
 	" *.Z *.elc *.ln core"
 	/* The rest we added to suit ourself. */
-	" .svn/ .git/ .bzr/";
+	" .svn/ .git/ .hg/ .bzr/";
 
 static void get_cvs_excludes(uint32 rflags)
 {
