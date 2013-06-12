0488a14b9930bf91719ac0f1d1c0c8770ca10646
Wayne Davison <wayned@samba.org>
2013-06-11 18:06:53 -0700

    Fix "make check".

--- t_stub.c
+++ t_stub.c
@@ -25,6 +25,7 @@ int modify_window = 0;
 int preallocate_files = 0;
 int protect_args = 0;
 int module_id = -1;
+int checksum_len = 0;
 int relative_paths = 0;
 int module_dirlen = 0;
 int preserve_acls = 0;
