4066d61a9dcc8a3c50d42fb9fbdd679c6ed0b3b0
Wayne Davison <wayned@samba.org>
2013-06-16 16:33:32 -0700

    Mention right option when using --delete-delay.

--- flist.c
+++ flist.c
@@ -1877,7 +1877,8 @@ static NORETURN void fatal_unsafe_io_error(void)
 	/* This (sadly) can only happen when pushing data because
 	 * the sender does not know about what kind of delete
 	 * is in effect on the receiving side when pulling. */
-	rprintf(FERROR_XFER, "FATAL I/O ERROR: dying to avoid a --delete-during issue with a pre-3.0.7 receiver.\n");
+	rprintf(FERROR_XFER, "FATAL I/O ERROR: dying to avoid a --delete-%s issue with a pre-3.0.7 receiver.\n",
+		delete_during == 2 ? "delay" : "during");
 	exit_cleanup(RERR_UNSUPPORTED);
 }
 
