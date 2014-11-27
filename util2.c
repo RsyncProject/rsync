5546dab32970955e77ef7a5886bcd8fb765a25bf
Wayne Davison <wayned@samba.org>
2014-11-27 11:57:35 -0800

    Use usleep() for msleep() if it is available.

--- util2.c
+++ util2.c
@@ -35,6 +35,9 @@ extern int checksum_len;
  **/
 int msleep(int t)
 {
+#ifdef HAVE_USLEEP
+	usleep(t*1000);
+#else
 	int tdiff = 0;
 	struct timeval tval, t1, t2;
 
@@ -48,11 +51,12 @@ int msleep(int t)
 		select(0,NULL,NULL, NULL, &tval);
 
 		gettimeofday(&t2, NULL);
-		if (t2.tv_sec < t1.tv_sec)
-			t1 = t2; /* Time went backwards, so start over. */
 		tdiff = (t2.tv_sec - t1.tv_sec)*1000 +
 			(t2.tv_usec - t1.tv_usec)/1000;
+		if (tdiff < 0)
+			t1 = t2; /* Time went backwards, so start over. */
 	}
+#endif
 
 	return True;
 }
