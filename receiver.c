3e06d40029cfdce9d0f73d87cfd4edaf54be9c51
Jeriko One <jeriko.one@gmx.us>
2017-11-02 23:44:19 -0700

    Check fname in recv_files sooner.

--- receiver.c
+++ receiver.c
@@ -574,6 +574,12 @@ int recv_files(int f_in, int f_out, char *local_name)
 			file = dir_flist->files[cur_flist->parent_ndx];
 		fname = local_name ? local_name : f_name(file, fbuf);
 
+		if (daemon_filter_list.head
+		    && check_filter(&daemon_filter_list, FLOG, fname, 0) < 0) {
+			rprintf(FERROR, "attempt to hack rsync failed.\n");
+			exit_cleanup(RERR_PROTOCOL);
+		}
+
 		if (DEBUG_GTE(RECV, 1))
 			rprintf(FINFO, "recv_files(%s)\n", fname);
 
@@ -644,12 +650,6 @@ int recv_files(int f_in, int f_out, char *local_name)
 		stats.total_transferred_size += F_LENGTH(file);
 
 		cleanup_got_literal = 0;
-
-		if (daemon_filter_list.head
-		    && check_filter(&daemon_filter_list, FLOG, fname, 0) < 0) {
-			rprintf(FERROR, "attempt to hack rsync failed.\n");
-			exit_cleanup(RERR_PROTOCOL);
-		}
 
 		if (read_batch) {
 			int wanted = redoing
