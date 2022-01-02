ffbca80ca2347beaa1004db56db439cdfa59f59e
Rodrigo Osorio <rodrigo@osorio.me>
2022-01-02 23:37:27 +0100

    Time-limit options are not being checked enough (#179)
    The `--stop-at`, `--stop-after`, and `--time-limit`` options should have their
    limit checked when receiving and sending data, not just when receiving.
    Fixes #177.

--- io.c
+++ io.c
@@ -801,18 +801,19 @@ static char *perform_io(size_t needed, int flags)
 					who_am_i(), (SIZE_T_FMT_CAST)n);
 			}
 
-			if (io_timeout || stop_at_utime) {
+			if (io_timeout) {
 				last_io_in = time(NULL);
-				if (stop_at_utime && last_io_in >= stop_at_utime) {
-					rprintf(FERROR, "stopping at requested limit\n");
-					exit_cleanup(RERR_TIMEOUT);
-				}
 				if (io_timeout && flags & PIO_NEED_INPUT)
 					maybe_send_keepalive(last_io_in, 0);
 			}
 			stats.total_read += n;
 
 			iobuf.in.len += n;
+		}
+
+		if (stop_at_utime && time(NULL) >= stop_at_utime) {
+			rprintf(FERROR, "stopping at requested limit\n");
+			exit_cleanup(RERR_TIMEOUT);
 		}
 
 		if (out && FD_ISSET(iobuf.out_fd, &w_fds)) {
