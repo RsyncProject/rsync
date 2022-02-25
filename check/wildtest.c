321e4f84b832a190af0d487833e2be21e77fd706
Sam Mikes <sam.mikes@gmail.com>
2022-02-24 18:45:07 -0700

    add libcheck test for iwildmatch
    create file iwildtest.txt for case-insensitive tests

--- check/wildtest.c
+++ check/wildtest.c
@@ -39,6 +39,41 @@ START_TEST(wildtest_legacyfile) {
     ck_assert_int_eq(line, 165);
 }
 
+START_TEST(wildtest_insensitive) {
+    FILE *fp = NULL;
+
+    char *file = "./check/iwildtest.txt";
+
+    char buf[2048], *string[2], *end[2];
+    int flag[2];
+
+    if ((fp = fopen(file, "r")) == NULL) {
+        ck_assert_msg(fp != NULL, "Unable to open %s", file);
+        return;
+    }
+
+    int line = 0;
+    while (fgets(buf, sizeof buf, fp)) {
+        line++;
+
+        if (parse_line(&file, line, buf, flag, end, string) == 0)
+            continue;
+
+        char *text = string[0];
+        char *pattern = string[1];
+        bool matches = flag[0];
+
+        bool matched = iwildmatch(pattern, text);
+
+        ck_assert_msg(
+            matched == matches,
+            "wildmatch failure on line %d:\n  %s\n  %s\n  expected %s match\n",
+            line, text, pattern, matches ? "a" : "NO");
+    }
+
+    ck_assert_int_eq(line, 165);
+}
+
 Suite *wildtest_suite() {
     Suite *s;
     TCase *tcase;
@@ -46,6 +81,10 @@ Suite *wildtest_suite() {
     s = suite_create("wildtest");
     tcase = tcase_create("wildtest - wildtest.txt");
     tcase_add_test(tcase, wildtest_legacyfile);
+    suite_add_tcase(s, tcase);
+
+    tcase = tcase_create("wildtest - iwildtest.txt (insensitive)");
+    tcase_add_test(tcase, wildtest_insensitive);
     suite_add_tcase(s, tcase);
 
     return s;
